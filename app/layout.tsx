c8b28d178fbd18eb3802ea993071af802c6d79ff
dbstj0403 <wcn09088@naver.com>
2025-06-01 14:05:13 +0900

    chore: meta data 추가 및 SessionProvider 분리

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,23 @@
-'use client';
-
-import { SessionProvider } from 'next-auth/react';
 import React from 'react';
 import './globals.css';
+import Providers from './providers';
+
+export const metadata = {
+  title: 'Digital Hanaro Tech Blog',
+  description: 'Digital Hanaro Tech Blog 💚',
+};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ko">
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard.css"
+        />
+      </head>
       <body>
-        <SessionProvider>{children}</SessionProvider>
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
