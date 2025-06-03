ff555baa0ca0d1442c207dbfaecdf6686a4a3e8b
dbstj0403 <wcn09088@naver.com>
2025-06-03 20:22:18 +0900

    feat: 마이페이지 세션 확인 로직 추가

--- app/my-page/page.tsx
+++ app/my-page/page.tsx
@@ -1,9 +1,14 @@
 import { getServerSession } from 'next-auth';
+import { redirect } from 'next/navigation';
 import { authOptions } from '@/lib/auth';
 import ProfileContent from './_components/ProfileContent';
 
 export default async function ProfilePage() {
   const session = await getServerSession(authOptions);
+
+  if (!session) {
+    redirect('/');
+  }
 
   return (
     <>
