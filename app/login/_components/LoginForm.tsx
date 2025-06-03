406c77938f78cb8ad5cff4699b66ca0600a02978
dbstj0403 <wcn09088@naver.com>
2025-06-03 15:24:41 +0900

    refactor: auth/me api 삭제 및 불필요한 모달 제거

--- app/login/_components/LoginForm.tsx
+++ app/login/_components/LoginForm.tsx
@@ -1,64 +1,55 @@
-"use client";
+'use client';
 
-import Image from "next/image";
-import ConfirmLinkModal from "@/app/auth/confirm-link/_components/ConfirmModal";
-import logo from "@/assets/icons/logoIcon.png";
-import useLogin from "@/hooks/login/useLogin";
+import Image from 'next/image';
+import logo from '@/assets/icons/logoIcon.png';
+import useLogin from '@/hooks/login/useLogin';
 
 const LoginForm = () => {
-  const {
-    formValues,
-    loading,
-    error,
-    handleChange,
-    handleCredentialsLogin,
-    handleGithubLogin,
-  } = useLogin();
+  const { formValues, loading, error, handleChange, handleCredentialsLogin, handleGithubLogin } =
+    useLogin();
 
   return (
     <>
-      <div className="max-w-md mx-auto p-6 space-y-6 pt-16 mt-10">
-        <div className="flex gap-2 items-center">
-          <Image src={logo} alt="logo" className="w-12 h-12" />
-          <p className="font-semibold text-sm">Digital Hanaro Tech</p>
+      <div className='max-w-md mx-auto p-6 space-y-6 pt-16 mt-10'>
+        <div className='flex gap-2 items-center'>
+          <Image src={logo} alt='logo' className='w-12 h-12' />
+          <p className='font-semibold text-sm'>Digital Hanaro Tech</p>
         </div>
-        <p className="text-2xl font-semibold text-gray-600">로그인</p>
+        <p className='text-2xl font-semibold text-gray-600'>로그인</p>
 
-        <form onSubmit={handleCredentialsLogin} className="space-y-8">
+        <form onSubmit={handleCredentialsLogin} className='space-y-8'>
           <div>
-            <label className="block mb-1 text-sm text-gray-500">
-              이메일 주소
-            </label>
+            <label className='block mb-1 text-sm text-gray-500'>이메일 주소</label>
             <input
-              name="email"
-              type="email"
+              name='email'
+              type='email'
               value={formValues.email}
               onChange={handleChange}
               required
-              className="w-full p-2 border rounded-lg"
+              className='w-full p-2 border rounded-lg'
             />
           </div>
 
           <div>
-            <label className="block mb-1 text-sm text-gray-500">비밀번호</label>
+            <label className='block mb-1 text-sm text-gray-500'>비밀번호</label>
             <input
-              name="password"
-              type="password"
+              name='password'
+              type='password'
               value={formValues.password}
               onChange={handleChange}
               required
-              className="w-full p-2 border rounded-lg"
+              className='w-full p-2 border rounded-lg'
             />
           </div>
 
-          {error && <p className="text-red-600 text-xs">{error}</p>}
+          {error && <p className='text-red-600 text-xs'>{error}</p>}
 
           <button
-            type="submit"
+            type='submit'
             disabled={loading}
-            className="w-full py-3 bg-hana-green text-white rounded-lg cursor-pointer"
+            className='w-full py-3 bg-hana-green text-white rounded-lg cursor-pointer'
           >
-            {loading ? "로그인 처리 중입니다..." : "로그인"}
+            {loading ? '로그인 처리 중입니다...' : '로그인'}
           </button>
         </form>
 
@@ -66,24 +57,17 @@ const LoginForm = () => {
 
         <button
           onClick={handleGithubLogin}
-          className="w-full py-3 bg-gray-800 text-white rounded-lg cursor-pointer"
+          className='w-full py-3 bg-gray-800 text-white rounded-lg cursor-pointer'
         >
           Github 계정으로 로그인
         </button>
         <p
-          className="text-sm text-gray-500 underline text-center cursor-pointer"
-          onClick={() => window.location.replace("/signup")}
+          className='text-sm text-gray-500 underline text-center cursor-pointer'
+          onClick={() => window.location.replace('/signup')}
         >
           회원가입
         </p>
       </div>
-
-      <ConfirmLinkModal
-        isOpen={showConfirmModal}
-        email={confirmEmail}
-        onConfirm={handleConfirmLink}
-        onCancel={handleCancelLink}
-      />
     </>
   );
 };
