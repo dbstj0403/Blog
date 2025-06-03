1f6f2dbc0a0cea079f14bc45960d61523fe74a80
dbstj0403 <wcn09088@naver.com>
2025-06-03 20:33:19 +0900

    feat: write 페이지 일반 유저일 경우 리다이렉트

--- app/write/page.tsx
+++ app/write/page.tsx
@@ -1,11 +1,18 @@
-import WriteForm from "./_components/WriteFrom";
+import { getServerSession } from 'next-auth';
+import { redirect } from 'next/navigation';
+import { authOptions } from '@/lib/auth';
+import WriteForm from './_components/WriteFrom';
 
-const WritePage = () => {
+export default async function WritePage() {
+  const session = await getServerSession(authOptions);
+
+  // 관리자가 아닐 경우 메인 화면으로 리다이렉트
+  if (!session || session.user.role !== 'ADMIN') {
+    redirect('/');
+  }
   return (
     <>
       <WriteForm />
     </>
   );
-};
-
-export default WritePage;
+}
